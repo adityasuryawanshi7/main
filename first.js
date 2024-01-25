a2a016540d1d89ca4b5f5849b32978ebbd034a2e
adityasuryawanshi7 <157221199+adityasuryawanshi7@users.noreply.github.com>
2024-01-25 19:02:40 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,4 +1,39 @@
-//only to modify current website , to see complete javascript : {ctrl+r}
+let a = document.getElementById("checking");
+console.log(a);
 
-alert("hellow! ");
+let div = document.querySelector("div")
+ 
+//changing element in html
+let h2=document.querySelector("h2")
+console.dir(h2.innerText);
+h2.innerText= h2.innerText + "by aditya";
+
+
+let t= document.querySelector("#t")
+    
+t.ondblclick= () => {
+    console.log("t button was clicked two times")
+    alert("you are stupid !")
+}
+
+
+let modeBtn = document.querySelector("#mode");
+let currMode="light";
+
+modeBtn.addEventListener("click", ()=> {
+    console.log("user trying to change mode");
+  
+    if(currMode==="light")
+    {currMode="dark";
+    document.querySelector("body").style.backgroundColor="black"
+    document.querySelector("body").style.color="white"
+}
+  
+    else
+    {currMode="light";
+    document.querySelector("body").style.backgroundColor="pink"
+}
+    console.log(currMode)
+});
+
 
